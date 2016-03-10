ee55bc33b4fbfeace03053bde17fa562d12f3e55
LI WENXIN <LIWENXIN@airbears2-10-142-11-158.airbears2.1918.berkeley.edu>
2016-03-09 20:02:46 -0800

    Add comments

--- UnionFind/src/WeighedQuickUnionDS.java
+++ UnionFind/src/WeighedQuickUnionDS.java
@@ -5,7 +5,7 @@ public class WeighedQuickUnionDS {
     private int[] parent;
     private int[] size;
 
-    public WeighedQuickUnionDS(int n) {
+    public WeighedQuickUnionDS(int n) {     // 两个array 一个存当前节点的父亲节点 一个存当前节点下面的元素个数
         parent = new int[n];
         size  = new int[n];
 
@@ -15,20 +15,20 @@ public class WeighedQuickUnionDS {
         }
     }
 
-    private int find(int p) {
+    private int find(int p) {               // 当找到当前节点等于他的父亲节点时(表示已经到根) 返回这个值
         if (p == parent[p]) {
             return p;
         } else {
-            parent[p] = find(parent[p]);
-            return parent[p];
+            parent[p] = find(parent[p]);    // 优化 在找这个节点的位置时 是从下往上找的 将经过的所有的节点都直接放到根节点下
+            return parent[p];               // 压缩树的高度 提升效率
         }
     }
 
     public boolean isConnected(int p, int q) {
         return find(p) == find(q);
-    }
+    } // 从下往上查一遍树
 
-    public void connect(int p, int q) {
+    public void connect(int p, int q) {     // 将两个节点连起来
         int i = find(p);
         int j = find(q);
 
@@ -42,3 +42,5 @@ public class WeighedQuickUnionDS {
         }
     }
 }
+
+/** 复杂度是logN */
